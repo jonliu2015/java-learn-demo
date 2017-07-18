3d958b31aca8948b7fd6a02d7dd05cdf78c12736
LIUZHENG <851688311@qq.com>
2017-07-18 20:30:53 +0800

    in dor do 1023 think of 1024 have idea no code

--- java-algorithm/src/main/java/edu/zju/liuz/pat/Main.java
+++ java-algorithm/src/main/java/edu/zju/liuz/pat/Main.java
@@ -759,11 +759,77 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String origin = scanner.next();
-
+        String trans = doubleNum(origin);
+        int[] count1 = new int[10];
+        int[] count2 = new int[10];
+        boolean yes = true;
+        for (char c: trans.toCharArray()) {
+            count2[c-'0']++;
+        }
+        for (char c: origin.toCharArray()) {
+            count1[c-'0']++;
+        }
+        for (int i = 0; i < 10; i++) {
+            if (count1[i]!=count2[i]) {
+                yes = false;
+                break;
+            }
+        }
+        if (yes) System.out.println("Yes");
+        else System.out.println("No");
+        System.out.println(trans);
     }
     public static String doubleNum(String num) {
         String result = "";
+        char[] cs = num.toCharArray();
+        int[] re = new int[cs.length + 1];
+        for (int i = 1; i < re.length; i++) {
+            re[i] = (cs[i - 1] - '0') * 2;
+        }
+        for (int i = 0; i < re.length - 1; i++) {
+            re[i] = re[i + 1]/10 + re[i];
+            re[i + 1] = re[i + 1]%10;
+        }
+        boolean start = false;
+        for (int i = 0; i < re.length; i++) {
+            if (re[i] > 0) start = true;
+            if (start) result += ""+re[i];
+        }
         return result;
     }
+
+    /**
+     1024. Palindromic Number (25)
+     A number that will be the same when it is written forwards or backwards is known as a Palindromic
+     Number. For example, 1234321 is a palindromic number. All single digit numbers are palindromic numbers.
+     Non-palindromic numbers can be paired with palindromic ones via a series of operations. First,
+     the non-palindromic number is reversed and the result is added to the original number. If the result
+     is not a palindromic number, this is repeated until it gives a palindromic number. For example, if we
+     start from 67, we can obtain a palindromic number in 2 steps: 67 + 76 = 143, and 143 + 341 = 484.
+     Given any positive integer N, you are supposed to find its paired palindromic number and the number
+     of steps taken to find it.
+     Input Specification:
+     Each input file contains one test case. Each case consists of two positive numbers N and K, where
+     N (<= 1010) is the initial numer and K (<= 100) is the maximum number of steps. The numbers are
+     separated by a space.
+     Output Specification:
+     For each test case, output two numbers, one in each line. The first number is the paired palindromic
+     number of N, and the second number is the number of steps taken to find the palindromic number. If
+     the palindromic number is not found after K steps, just output the number obtained at the Kth step
+     and K instead.
+     Sample Input 1:
+     67 3
+     Sample Output 1:
+     484
+     2
+     Sample Input 2:
+     69 3
+     Sample Output 2:
+     1353
+     3
+     思路：1.步骤次数内做题目所给运算；2.循环判断是否Palindromic Number；
+     * */
+
+    
 
 }
