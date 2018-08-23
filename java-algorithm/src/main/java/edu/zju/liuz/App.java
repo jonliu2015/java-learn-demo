adb09060277cbcde4075ad4a5964db28e1a180da
liuz <851688311@qq.com>
2018-08-23 14:59:43 +0800

    修复代码错误

--- java-algorithm/src/main/java/edu/zju/liuz/App.java
+++ java-algorithm/src/main/java/edu/zju/liuz/App.java
@@ -2,9 +2,6 @@ package edu.zju.liuz;
 
 import edu.zju.liuz.leetcode.Solution;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 /**
  * Hello world!
  *
@@ -13,7 +10,6 @@ public class App
 {
     public static void main( String[] args ) {
         Solution solution = new Solution();
-        Arrays.binarySearch()
         System.out.println(solution.longestPalindrome("babad"));
     }
 }
